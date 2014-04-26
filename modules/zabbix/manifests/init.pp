ac9e13df6941b33a5b5f89de1da3c3c47aece414
George Souza Farias <georgesouzafarias@gmail.com>
2014-04-26 18:58:50 -0400

    + apt Mudules + apt key + stable

--- modules/zabbix/manifests/init.pp
+++ modules/zabbix/manifests/init.pp
@@ -1,5 +1,21 @@
+include apt
+
 class zabbix {
-  package { 'zabbix-agent': ensure => installed, }
+  apt::key { 'zabbix_key': key_source => 'http://repo.zabbix.com/zabbix-official-repo.key', 
+  }
+
+  apt::source { 'zabbix_repository':
+    location    => 'http://repo.zabbix.com/zabbix/2.2/debian',
+    repos       => 'main',
+    include_src => false,
+    require     => Apt::Key['zabbix_key'],
+  }
+
+  package { 'zabbix-agent':
+    ensure   => installed,
+    provider => aptitude,
+    require  => Apt::Source['zabbix_repository'],
+  }
 
   # Send the configuration files
   file { "zabbix_agentd.conf":
@@ -16,52 +32,10 @@ class zabbix {
     require => File['zabbix_agentd.conf'],
     enable  => true,
   }
+}
+# Send the archives correct for osfamily and architecture.
 
-  # Send the archives correct for osfamily and architecture.
-
-  # if $::osfamily == 'Debian' {
-  #  $extensao = 'deb'
-  # } else {
-  # $extensao = 'rpm'
-  #}
-
-  # $pacote = "zabbix-agent_${::osfamily}_${::architecture}.${extensao}"
-
-  # file { "zabbix-package":
-  #  path   => "/tmp/${pacote}",
-  #  ensure => file,
-  #  source => "puppet:///zabbix/files/${pacote}",
-  #}
-
-  # user { 'zabbix':
-  # ensure  => present,
-  # groups  => 'zabbix',
-  # require => Group['zabbix'],
-  #}
-
-  # group { 'zabbix': ensure => present, }
-
-
-  # Check the osfamily and send the script daemon correct
-  #  case $::osfamily {
-  #    'Debian', 'RedHat' : {
-  #      file { "zabbix-agent":
-  #        path    => "/etc/init.d/zabbix-agent",
-  #        ensure  => file,
-  #        owner   => root,
-  #        group   => root,
-  #        content =>
-  #        "puppet:///zabbix/files/init/zabbix-agent-${::osfamily}.conf",
-  #  }
-  #}
-  #  default            : {
-  #    fail('OS nao suportado')
-  #  }
-  #}
-
-  # exec { 'apt-get':
-  #   command => "/usr/bin/apt-get -f install -y",
-  #   unless  => "/usr/bin/apt-get check",
-  #}
-
-  # installing the zabbix-agent package
+# if $::osfamily == 'Debian' {
+#  $extensao = 'deb'
+# } else {
+# $extensao = 'rpm'
